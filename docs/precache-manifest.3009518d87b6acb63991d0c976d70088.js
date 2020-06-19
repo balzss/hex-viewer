b6ef27c2c65a089eec390c2d9b6d8e8df3a97b66
balazs.saros <balazs.saros@gmail.com>
2020-06-19 17:09:44 +0200

    docs folder

---
+++ docs/precache-manifest.3009518d87b6acb63991d0c976d70088.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae59cf43d726e64250d9d53314ff308e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de617a60e09d323102b9",
+    "url": "/static/css/main.829da48c.chunk.css"
+  },
+  {
+    "revision": "6e84e8806d4b3918574e",
+    "url": "/static/js/2.ade4956d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ade4956d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de617a60e09d323102b9",
+    "url": "/static/js/main.de5ae39f.chunk.js"
+  },
+  {
+    "revision": "d8e37ea4c1718b84e6ee",
+    "url": "/static/js/runtime-main.dbf91dfb.js"
+  }
+]);
